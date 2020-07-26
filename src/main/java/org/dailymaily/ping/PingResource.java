341d5e813cb8f2a4f9b1c12d8b0fce991fd1791c
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2020-07-26 19:54:42 +0200

    Add ping resource

--- src/main/java/org/dailymaily/ping/PingResource.java
+++ src/main/java/org/dailymaily/ping/PingResource.java
@@ -1,16 +1,16 @@
-package org.acme;
+package org.dailymaily.ping;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class ExampleResource {
+@Path("/ping")
+public class PingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "ping";
     }
 }
--- src/test/java/org/dailymaily/ping/PingResourceTest.java
+++ src/test/java/org/dailymaily/ping/PingResourceTest.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.dailymaily.ping;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +7,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class PingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testPingEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/ping")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("ping"));
     }
 
 }
